891954901a0bc4e98be7d151c17d247fff8b50ce
UdayBari3421 <udaybari2003@gmail.com>
2024-10-06 15:09:32 +0530

    added simple validation

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,23 +1,29 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 
 function Modal({ className, style, setFilterdUser, filterdUser, setModal }) {
+  const [eror, setError] = useState("");
+
   const handleCreateSubmit = async (e) => {
     e.preventDefault();
     const user = {
-      name: e.target[0].value,
-      email: e.target[1].value,
-      phone: e.target[2].value,
-      website: e.target[3].value,
+      name: e.target.name.value,
+      email: e.target.email.value,
+      phone: e.target.phone.value,
+      website: e.target.website.value,
     };
 
-    const response = await axios.post("https://jsonplaceholder.typicode.com/users", user);
-    const newUser = response.data;
-    const newData = [...filterdUser, newUser];
-
-    setFilterdUser(newData);
-
-    setModal(false);
+    if (!user.name || !user.email || !user.phone || !user.website) {
+      setError("Please fill all the fields");
+    } else {
+      try {
+        const response = await axios.post("https://jsonplaceholder.typicode.com/users", user);
+        setFilterdUser([...filterdUser, response.data]);
+        setModal(false);
+      } catch (error) {
+        setError(error);
+      }
+    }
   };
 
   return (
@@ -27,11 +33,11 @@ function Modal({ className, style, setFilterdUser, filterdUser, setModal }) {
           <h1>Create User</h1>
           <button onClick={(prev) => setModal(!prev)}>X</button>
         </span>
-
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Phone" />
-        <input type="text" placeholder="Website" />
+        {eror && <div className="error">Error</div>}
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="text" name="phone" placeholder="Phone" />
+        <input type="text" name="website" placeholder="Website" />
         <button type="submit">Create</button>
       </form>
     </div>
